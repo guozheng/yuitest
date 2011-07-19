1052f015a96ec55856215b4e45aabd242fb0683b
Guozheng Ge <guozheng.ge@gmail.com>
2011-07-19 14:31:22 -0700

    add -x command line option to exclude directories and files from code instrumentation
    -x or --exclude <file path OR directory path>
    
    Notes:
    
    1. -x only works when input is a directory
    2. path is relative to the input directory
    3. use multiple -x <value> to exclude multiple paths
    4. excluded paths are still copied over to make sure tests still work, they are just not instrumented by yuitest-coverage
    
    Examples:
    
    yuitest-coverage -d -o build -x node_modules -x middlewares/timeout.js src/main/javascript
    
    This will skip instrumentation for src/main/javascript/node_modules and src/main/javascript/middleware/timeout.js

--- java/src/com/yahoo/platform/yuitest/coverage/DirectoryInstrumenter.java
+++ java/src/com/yahoo/platform/yuitest/coverage/DirectoryInstrumenter.java
@@ -11,9 +11,14 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+
 import org.antlr.runtime.RecognitionException;
+
+import org.apache.commons.io.FileUtils;
 
 
 /**
@@ -32,7 +37,7 @@ public class DirectoryInstrumenter {
         DirectoryInstrumenter.verbose = verbose;
     }
 
-    public static void instrument(String inputDir, String outputDir) 
+    public static void instrument(String inputDir, String outputDir, HashSet<String> excludes) 
             throws FileNotFoundException, UnsupportedEncodingException,
             IOException, RecognitionException {
 
@@ -44,7 +49,7 @@ public class DirectoryInstrumenter {
             outputDir = outputDir + File.separator;
         }
 
-        List<String> filenames = getFilenames(inputDir);
+        List<String> filenames = getFilenames(inputDir, excludes);
 
         for (int i=0; i < filenames.size(); i++){
             String inputFilename = filenames.get(i);
@@ -64,10 +69,40 @@ public class DirectoryInstrumenter {
             FileInstrumenter.setVerbose(verbose);
             FileInstrumenter.instrument(inputFilename, outputFilename);
         }
-
+        
+        //copy files and directories excluded from instrumentation, still need them to run tests
+        copyExcludes(inputDir, outputDir, excludes);
     }
 
-
+    /**
+     * Copy files and directories excluded for instrumenation to destination directory.
+     * @param inputDir String The source directory containing the excluded file or directory
+     * @param outputDir String The destination directory
+     * @param excludes HashSet A HashSet containing the file and directory paths to exclude from instrumentation.
+     * 		These paths are in the form of inputDir+filename/dirname.
+     * @throws IOException various IO errors including null or invalid source, destination, etc.
+     */
+    private static void copyExcludes(String inputDir, String outputDir, HashSet<String> excludes) throws IOException {
+    	//copy files and directories excluded for instrumentation, need them to run tests properly
+    	String name = null;
+    	String destName = null;
+    	File f = null;
+    	
+    	System.out.println("intput dir=" + inputDir);
+    	
+        for (Iterator<String> it = excludes.iterator(); it.hasNext(); ) {
+        	name = it.next();
+        	destName = outputDir + name.substring(inputDir.length());
+        	f = new File(name);
+        	if (f.isFile()) {
+        		System.out.println("copying skipped file " + name + " to " + destName);
+        		FileUtils.copyFile(f, new File(destName));
+        	} else if (f.isDirectory()) {
+        		System.out.println("copying skipped directory " + name + " to " + destName);
+        		FileUtils.copyDirectory(f, new File(destName));
+        	}
+        }
+    }
 
     /**
      * Retrieves a recursive list of all JavaScript files in the inputDir.
@@ -76,11 +111,14 @@ public class DirectoryInstrumenter {
      * @throws IllegalArgumentException When the inputDir cannot be read.
      * @throws FileNotFoundException When the inputDir doesn't exist.
      */
-    private static List<String> getFilenames(String directory) throws IllegalArgumentException, FileNotFoundException {
-
-        File dir = new File(directory);
+    private static List<String> getFilenames(String directory, HashSet<String> excludes) throws IllegalArgumentException, FileNotFoundException {
+    	
+    	File dir = new File(directory);
 
         //validate the inputDir first
+        if (!dir.exists()){
+        	throw new FileNotFoundException("'" + directory + "' does not exist.");
+        }
         if (!dir.isDirectory()){
             throw new FileNotFoundException("'" + directory + "' is not a valid directory.");
         }
@@ -91,18 +129,17 @@ public class DirectoryInstrumenter {
         List<String> filenames = new LinkedList<String>();
         
         //TODO: Gotta be a better way...
-        File[] files = dir.listFiles();
+        File[] files = dir.listFiles(new FileDirFilter(excludes));
         for (int i=0; i < files.length; i++){
             if (files[i].isFile() && files[i].getName().endsWith(".js")){
                 filenames.add(files[i].getPath());
             } else if (files[i].isDirectory()){
-                filenames.addAll(getFilenames(files[i].getPath()));
+                filenames.addAll(getFilenames(files[i].getPath(), excludes));
             }
         }
 
         return filenames;
 
     }
-
 
 }
